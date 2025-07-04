90f28d7d3646814d8f2d4e08a9b26789e562a462
jor <jor@debian.jor>
2025-07-03 21:08:52 -0300

    carpeta vacia

--- TPSP/src/idle.asm
+++
@@ -1,9 +0,0 @@
-; ==============================================================================
-; TALLER System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
-; ==============================================================================
-
-ORG 0x0001C000 ; TASK_IDLE_CODE
-
-BITS 32
-
-idle: jmp idle
